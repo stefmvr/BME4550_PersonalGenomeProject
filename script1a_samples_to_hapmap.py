4ea3b2c8734ccbdf11a19f7b4175954988cb90d0
stefmvr <stef.mvr@gmail.com>
2016-04-25 11:40:35 -0400

    scripts round 1

---
+++ script1a_samples_to_hapmap.py
@@ -0,0 +1,118 @@
+# Personal Genome Script 1A 
+
+# Helper function to synchronize SNPs
+def filter_missing_snps(snpSrc, snpRef):
+	to_remove = []
+	for each in snpSrc:
+		if each not in snpRef:
+			to_remove.append(each)
+	for each in to_remove:
+		del snpSrc[each]
+	return snpSrc
+
+
+# Helper Function to process a sample genome file
+def process_sample(filename):
+	snpMap = {}
+	with open(filename) as sample_file:
+		for each in sample_file:
+			# If not a comment
+			if each[0] != "#":
+				# Parse the line and map rsid -> genotype call
+				inline = each.strip().split()
+				snpMap[inline[0]] = inline[3]
+	return snpMap
+
+
+# Helper Function to write a sample to a .ped file
+def write_to_ped(pedfile, sample_id, sample_reads):
+	pedfile.write(sample_id + "\t" + sample_id + "\t0\t0\t1\t-9\t")
+	# For each SNP in the .map file
+	with open("samples_hapmap.map") as mapfile:	
+		for each in mapfile:
+			rsid = each.strip().split()[1]
+			# If the rsid is in the sample and has a call
+			if rsid in sample_reads and '-' not in sample_reads[rsid]:
+				call = sample_reads[rsid]
+				# If the call has both alleles, write them to the .ped file
+				if len(call) > 1:
+					pedfile.write(call[0] + " " + call[1] + "\t")
+				# If the call has only one allele, write as if homozygous
+				else:
+					pedfile.write(call[0] + " " + call[0] + "\t")
+			# If the rsid is not in the sample, then write 0 0 to indicate a missing call
+			else:
+				pedfile.write("0 0\t")
+		pedfile.write("\n")
+
+
+
+# Process sample genomes
+snpA = process_sample("genome_A.txt")
+print(len(snpA))
+snpB = process_sample("genome_B.txt")
+print(len(snpB))
+snpC = process_sample("genome_C.txt")
+print(len(snpC))
+
+
+# Filter sample maps to include only shared SNPs
+snpA = filter_missing_snps(snpA, snpB)
+snpA = filter_missing_snps(snpA, snpC)
+snpB = filter_missing_snps(snpB, snpA)
+snpC = filter_missing_snps(snpC, snpA)
+
+
+rscheck = {}
+with open("hapmap3.map") as mapfile:
+	count = 0
+	for each in mapfile:
+		mapline = each.strip().split()
+		# If the RSID is in our shared SNP set
+		if mapline[1] in snpA:
+			# Map RSID -> .map position
+			rscheck[mapline[1]] = count
+		count = count + 1
+print("rs check length:")
+print(len(rscheck))
+
+
+# Remove SNPs not present in the hapmap file
+filter_missing_snps(snpA, rscheck)
+filter_missing_snps(snpB, rscheck)
+filter_missing_snps(snpC, rscheck)
+
+
+# Write new map file, filtering out non-common SNPs
+print("writing samples_hapmap")
+hapmap_lines = []
+with open("hapmap3.map") as infile, open("samples_hapmap.map", 'w') as outfile:
+	onsnps = 0
+	offsnps = 0
+	for each in infile:
+		inline = each.strip().split()
+		value = int(inline[3])
+		# If the SNP is a common SNP, then write back the original line
+		if inline[1] in rscheck:
+			outfile.write(each)
+		# If the SNP is not a common SNP, replace the base-position field with a negative number
+		else:
+			value = value * -1	
+			newline = inline[0] + "\t" + inline[1] + "\t" + inline[2] + "\t" + str(value) + "\n"
+			outfile.write(newline)
+
+		# Count the number of valid and invalid SNPs for processing
+		if value > 0:
+			onsnps = onsnps + 1
+		else:
+			offsnps = offsnps + 1
+	print(onsnps)
+	print(offsnps)				
+
+
+# Write the sample genome ped file
+print("writing ped")
+with open("samples_hapmap.ped", 'w') as pedfile:
+	write_to_ped(pedfile, "A", snpA)
+	write_to_ped(pedfile, "B", snpB)
+	write_to_ped(pedfile, "C", snpC)
---
+++ script1b_filter_tri_alleles.py
@@ -0,0 +1,71 @@
+# Personal Genome Script 1B
+
+# All SNPs in the .map file, rsid index -> rsid
+rsids = {}
+# Only SNPs that will be processed, mapping rsid index -> rsid
+rsids_to_track = {}
+
+# Read in the .map file
+with open("samples_hapmap.map") as mapfile:
+	count = 0
+	for each in mapfile:
+		# Make a map of rsid index -> rsid
+		rsid = each.strip().split()[1]
+		rsids[count] = rsid
+		if int(each.strip().split()[3]) >= 0:
+			rsids_to_track[count] = rsid
+		count = count + 1
+
+num_rsids = len(rsids)
+print(len(rsids_to_track))
+print(num_rsids)
+
+rsid_allele = {}
+# Open the combined .ped file for checking for tri-alleles
+with open ("hapmapandme.ped") as pedfile:
+	for each in pedfile: 
+		pedline = each.strip().split()[6:]
+		# For each SNP in the individual's genome
+		for i in range(num_rsids):
+			# If the SNP is one of the common SNPs we are processing
+			if i in rsids_to_track:
+				current_snp = rsids_to_track[i]
+				# If the SNP has not yet been added, add it to the rsid_allele map
+				if current_snp not in rsid_allele:
+					rsid_allele[current_snp] = [] 
+				
+				# If either allele is not in the rsid_allele mapping, add them
+				# Don't add the allele if it is unknown (0)
+				if pedline[i*2] not in rsid_allele[current_snp] and pedline[i*2] != '0':
+					rsid_allele[current_snp].append(pedline[i*2])
+				if pedline[i*2+1] not in rsid_allele[current_snp] and pedline[i*2+1] != '0':
+					rsid_allele[current_snp].append(pedline[i*2+1])
+
+
+# Write new .map file and eliminate tri-allelic SNPs 
+print(len(rsid_allele))
+print("writing to file")
+with open("samples_hapmap.map") as infile, open("hapmapandme.map", 'w') as outfile:
+	onsnps = 0
+	offsnps = 0
+	# For each SNP in the mapfile
+	for each in infile:
+		inline = each.strip().split()
+		rsid = inline[1]
+		value = int(inline[3])
+		# If the rsid is in the rsid_allele map, has more than 2 alleles, and is still a valid SNP
+		if rsid in rsid_allele and len(rsid_allele[rsid]) > 2 and value > 0:
+			# Negate the value and write the new value to file 
+			value = value * -1
+			outfile.write(inline[0] + "\t" + inline[1] + "\t" + inline[2] + "\t" + str(value) + "\n")
+		else:
+			# Write the original line to file
+			outfile.write(each)
+
+		# Count the valid or invalid SNPs
+		if value > 0:
+			onsnps = onsnps + 1
+		else:
+			offsnps = offsnps + 1
+	print(onsnps)
+	print(offsnps)
